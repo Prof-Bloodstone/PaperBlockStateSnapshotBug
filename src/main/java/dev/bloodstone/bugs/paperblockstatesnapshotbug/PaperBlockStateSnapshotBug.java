58413b742a6d0ee9ed6932422c0650175a7aaa10
Professor Bloodstone <prof.bloodstone+git@gmail.com>
2020-07-27 20:41:16 +0200

    Add more debug info

--- src/main/java/dev/bloodstone/bugs/paperblockstatesnapshotbug/PaperBlockStateSnapshotBug.java
+++ src/main/java/dev/bloodstone/bugs/paperblockstatesnapshotbug/PaperBlockStateSnapshotBug.java
@@ -1,5 +1,6 @@
 package dev.bloodstone.bugs.paperblockstatesnapshotbug;
 
+import javafx.util.Pair;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.TileState;
@@ -9,6 +10,8 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Arrays;
 
 public final class PaperBlockStateSnapshotBug extends JavaPlugin implements Listener, CommandExecutor {
 
@@ -20,20 +23,38 @@ public final class PaperBlockStateSnapshotBug extends JavaPlugin implements List
     @EventHandler
     public void onBlockPlaceEvent(BlockPlaceEvent event) {
         if (event.getBlockPlaced().getType() != Material.PLAYER_HEAD) return;
-        getServer().broadcastMessage("Checking snapshot...");
-        checkTileState(event.getBlock(), true);
-        getServer().broadcastMessage("Checking live state...");
-        checkTileState(event.getBlock(), false);
+        PersistentDataContainer snapshotPDC = checkTileState(event.getBlock(), true);
+        PersistentDataContainer livePDC = checkTileState(event.getBlock(), false);
+        if (snapshotPDC.equals(livePDC)) {
+            getServer().broadcastMessage("PDCs are not equal!");
+        } else getServer().broadcastMessage("PDCs are equal!");
+        for (Pair<String, PersistentDataContainer> p: Arrays.asList(
+                new Pair<>("Snapshot", snapshotPDC),
+                new Pair<>("Live", livePDC)
+        )) {
+            String name = p.getKey();
+            PersistentDataContainer pdc = p.getValue();
+            if (pdc == null) {
+                getServer().broadcastMessage(name + " PDC is NULL!");
+            } else if (pdc.isEmpty()) {
+                getServer().broadcastMessage(name + " is empty!");
+            } else {
+                getServer().broadcastMessage(name + " keys:");
+                pdc.getKeys().forEach( it ->getServer().broadcastMessage(it.toString()));
+            }
+        }
     }
 
-    private void checkTileState(Block block, boolean useSnapshot) {
+    private PersistentDataContainer checkTileState(Block block, boolean useSnapshot) {
         TileState state = (TileState) block.getState(useSnapshot);
+        getServer().broadcastMessage(String.format("Checking %s state...", (useSnapshot ? "snapshot" : "live")));
         PersistentDataContainer pdc = state.getPersistentDataContainer();
         if (pdc == null) {
             getServer().broadcastMessage("PDC is NULL");
         } else {
             getServer().broadcastMessage("PDC is NOT NULL");
         }
+        return pdc;
     }
 
     @Override
